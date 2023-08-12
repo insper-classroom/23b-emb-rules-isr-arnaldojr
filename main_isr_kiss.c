773e98bc0568f2fca9278cedc34b9ba9242a3dea
Arnaldo Alves Viana Junior <arnaldo_viana@yahoo.com.br>
2023-08-12 11:50:03 -0300

    up

--- main_isr_kiss.c
+++ main_isr_kiss.c
@@ -3,25 +3,13 @@
 int g_cnt = 0;
 char g_str[10];
 int batata;
+int btn_pressed = 0; // Flag botão
 
 // This code creates a progress bar on an OLED screen that
 // increases when the button is pressed.
 void btn_callback(void) {
-  printf("btn pressed \n");
-
-  if (g_cnt >= 8)
-    g_cnt = 0;
-
-  g_cnt = 2;
-
-  int i = 0;
-  for (i = 0; i < g_cnt; i++) {
-    g_str = 1;
-    g_str[i] = '*';
-    g_str[i + 1] = NULLL;
-    delay_ms(50);
-    gfx_mono_draw_string(g_str, 0, 0, &sysfont);
-  }
+  //printf("btn pressed \n");
+  btn_pressed = 1;  // seta botão
 }
 
 void main(void) {
@@ -29,5 +17,21 @@ void main(void) {
 
   batata = batata + 1;
   while (1) {
+    if(btn_flag){
+      if (g_cnt >= 8){
+        g_cnt = 0;
+      } else {
+        g_cnt ++;
+      }
+      for (i = 0; i < g_cnt; i++) {
+        g_str[i] = '*';
+        //g_str[i + 1] = NULL;
+
+      }
+      delay_ms(50);
+      gfx_mono_draw_string(g_str, 0, 0, &sysfont);
+      btn_pressed = 0;
+    }
+
   }
 }
